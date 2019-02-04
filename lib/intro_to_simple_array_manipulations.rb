221021300ccf6e18fabbc101a1c2d564e78fe941
Sean Kelly <shonkelli@gmail.com>
2019-02-04 22:28:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,4 +14,15 @@ def pop_with_args(array)
   array.pop(2)
 end 
 
-def
+def using_shift(array)
+  array.shift 
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end 
+
+def using_concat(array1, array2)
+  array.concat(array1, array2)
+end
+
